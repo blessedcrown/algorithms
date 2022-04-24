ed8f06fc12da9de43c4bbc121aaf4832d42619bf
blessedcrown <blessedcrown@naver.com>
2022-04-24 14:21:30 +0000

    백준 1074번

--- recursion/z.py
+++ recursion/z.py
@@ -1,3 +1,4 @@
+#https://www.acmicpc.net/problem/1074
 n, r, c = map(int, input().split())
 ans = 0
 
@@ -9,6 +10,8 @@ def z(n, r, c):
         else:
             ans += r+c+1
         return
+    #2X2가 아닌이상 매번 배열을 4등분하여 해당 인덱스가 1,2,3,4 분면중
+    #어디에 해당되는지 확인. 그후 index도 같이 줄여준다
     line = 2**(n-1)
     #4사분면
     if r >= line and c >= line:
